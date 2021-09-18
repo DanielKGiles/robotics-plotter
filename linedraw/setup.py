e3a6c6f6b5b9f943c859c90cf7b313c83633cefb
DanielKGiles <dgiles4@lion.lmu.edu>
2021-09-17 19:44:40 -0700

    Re-adding linedraw package

---
+++ linedraw/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='linedraw',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='LingDong Huang',
+    author_email='lingdong0618@hotmail.com',
+    description='Converts images to vectorized line drawings for plotters.'
+)
